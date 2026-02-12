6c1fe5e03f5552d31735aaae9d56bfbace06fb97
Rafal Tatun <rafal.tatun@globallogic.com>
2026-02-12 12:40:49 +0100

    feat: First update app with new urls
    Issue-ID: 1

--- app/src/games/views.py
+++ app/src/games/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .utils import get_games_from_sheet
 
-# Create your views here.
+def game_list(request):
+    games = get_games_from_sheet()
+    return render(request, 'games/game_list.html', {'games': games})
