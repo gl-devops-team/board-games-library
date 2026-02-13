67e09cfc5c36528bca3a58732ed13c55cad69456
Rafal Tatun <rafal.tatun@globallogic.com>
2026-02-13 11:11:47 +0100

    fix: Changed import boardgames from google sheet in to xlsx file
    ISSUE_ID: 1

--- app/src/games/views.py
+++ app/src/games/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from .utils import get_games_from_sheet
+from .utils import get_games_from_xlsx
 
 def game_list(request):
-    games = get_games_from_sheet()
+    games = get_games_from_xlsx("Wroclaw_list of boardgames.xlsx")
     return render(request, 'games/game_list.html', {'games': games})
