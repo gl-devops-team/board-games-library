a44c05b78c381b761a25a6dfc37f0d6c79e216fb
Rafal Tatun <rafal.tatun@globallogic.com>
2026-02-19 12:48:06 +0100

    fix: Changed structure convention, ReactJS and frontend added
    Issue_id: gh-1

---
+++ app/backend/games/urls.py
@@ -0,0 +1,15 @@
+# # games/urls.py
+# from django.urls import path
+
+# from games import views
+
+# urlpatterns = [
+#     path("", views.game_list, name="game-list"),
+# ]
+
+from django.urls import path
+from .views import game_list
+
+urlpatterns = [
+    path('games/', game_list),
+]
---
+++ app/frontend/src/App.jsx
@@ -0,0 +1,81 @@
+import './App.css'
+import { useEffect, useState } from "react";
+
+function App() {
+  const [games, setGames] = useState([]);
+  const [search, setSearch] = useState("");
+  const [sortConfig, setSortConfig] = useState(null);
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/api/games/")
+      .then(res => res.json())
+      .then(data => setGames(data));
+  }, []);
+
+  // Filtrowanie
+  const filteredGames = games.filter(game =>
+    game.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+  // Sortowanie
+  const sortedGames = [...filteredGames].sort((a, b) => {
+    if (!sortConfig) return 0;
+
+    const { key, direction } = sortConfig;
+    if (a[key] < b[key]) return direction === "asc" ? -1 : 1;
+    if (a[key] > b[key]) return direction === "asc" ? 1 : -1;
+    return 0;
+  });
+
+  const handleSort = (key) => {
+    setSortConfig({
+      key,
+      direction:
+        sortConfig?.key === key && sortConfig.direction === "asc"
+          ? "desc"
+          : "asc"
+    });
+  };
+
+  return (
+    <div className="container">
+      <h1>Lista gier planszowych</h1>
+
+      <input
+        type="text"
+        placeholder="Szukaj gry po nazwie..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+
+      <table>
+        <thead>
+          <tr>
+            <th onClick={() => handleSort("name")}>Nazwa ▲▼</th>
+            <th onClick={() => handleSort("players")}>Liczba graczy ▲▼</th>
+            <th onClick={() => handleSort("time")}>Czas gry ▲▼</th>
+            <th onClick={() => handleSort("description")}>Kategoria ▲▼</th>
+          </tr>
+        </thead>
+        <tbody>
+          {sortedGames.length > 0 ? (
+            sortedGames.map((game, index) => (
+              <tr key={index}>
+                <td>{game.name}</td>
+                <td>{game.players}</td>
+                <td>{game.time}</td>
+                <td>{game.description}</td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan="4">Brak gier do wyświetlenia</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
