2c7298b9a80e2fd2a7f73d19750b3f771dc20104
Rafal Tatun <rafal.tatun@globallogic.com>
2026-02-19 15:00:07 +0100

    fix: ruff format completed
    Issue_id: gh-1

--- app/backend/games/urls.py
+++ app/backend/games/urls.py
@@ -12,5 +12,5 @@ from django.urls import path
 from games.views import game_list
 
 urlpatterns = [
-    path('games/', game_list, name="game-list"),
+    path("games/", game_list, name="game-list"),
 ]
--- app/backend/games/views.py
+++ app/backend/games/views.py
@@ -5,7 +5,8 @@ from games.utils import get_games_from_xlsx
 
 SHEET_NAME = "Wroclaw_list of boardgames.xlsx"
 
-@api_view(['GET'])
+
+@api_view(["GET"])
 def game_list(request):
     games = get_games_from_xlsx(SHEET_NAME)
     return Response(games)
