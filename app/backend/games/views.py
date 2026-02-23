3b770721bf87a0df594528507855c956d551d78c
rtatun-GL <rafal.tatun@globallogic.com>
2026-02-23 11:30:22 +0100

    fix: improve project setup (#28)
    This PR fix structure and ReactJS, frontend added to the project.
    
    Issue-ID: gh-1
    
    ---------
    
    Co-authored-by: Krzysztof Szpieg <krzysztof.szpieg@globallogic.com>

--- app/backend/games/views.py
+++ app/backend/games/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from games.utils import get_games_from_xlsx
 
 SHEET_NAME = "Wroclaw_list of boardgames.xlsx"
 
 
+@api_view(["GET"])
 def game_list(request):
     games = get_games_from_xlsx(SHEET_NAME)
-    return render(request, "games/game_list.html", {"games": games})
+    return Response(games)
