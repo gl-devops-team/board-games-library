a22b09842f63ccc4526edfbd243b54ace9d16210
Rafal Tatun <rafal.tatun@globallogic.com>
2026-03-03 14:20:38 +0100

    Fix: Solved problem with backend and frontend connection
    Issue_ID: gh-30

--- app/backend/games/views.py
+++ app/backend/games/views.py
@@ -1,12 +1,14 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.conf import settings
+import os
 
 from games.utils import get_games_from_xlsx
 
-SHEET_NAME = "Wroclaw_list of boardgames.xlsx"
-
+FILE_NAME = "boardgamelist.xlsx"
 
 @api_view(["GET"])
 def game_list(request):
-    games = get_games_from_xlsx(SHEET_NAME)
+    file_path = os.path.join(settings.BASE_DIR, FILE_NAME)
+    games = get_games_from_xlsx(file_path)
     return Response(games)
