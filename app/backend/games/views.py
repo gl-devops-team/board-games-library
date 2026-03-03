70b9543b42944c4ab257320fc59a84b4449e408b
Rafal Tatun <rafal.tatun@globallogic.com>
2026-03-03 14:40:43 +0100

    Fix: Problem with file and path solved
    Issue_ID: gh-30

--- app/backend/games/views.py
+++ app/backend/games/views.py
@@ -1,14 +1,12 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from django.conf import settings
-import os
 
 from games.utils import get_games_from_xlsx
 
-FILE_NAME = "Wroclaw_list of boardgames.xlsx"
+SHEET_NAME = "Wroclaw_list of boardgames.xlsx"
+
 
 @api_view(["GET"])
 def game_list(request):
-    file_path = os.path.join(settings.BASE_DIR, FILE_NAME)
-    games = get_games_from_xlsx(file_path)
+    games = get_games_from_xlsx(SHEET_NAME)
     return Response(games)
