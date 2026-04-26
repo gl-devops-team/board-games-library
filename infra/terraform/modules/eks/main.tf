38469c498774854f476bb65fb7518b0ce4c66057
DariuszDrozdek <dariusz.drozdek@globallogic.com>
2026-04-26 10:42:04 +0200

    fix: pin EBS CSI driver addon version to v1.38.1-eksbuild.1
    Prevent silent addon upgrades on terraform apply by explicitly
    setting addon_version.
    
    Issue-ID: gh-89

--- infra/terraform/modules/eks/main.tf
+++ infra/terraform/modules/eks/main.tf
@@ -127,8 +127,9 @@ resource "aws_eks_node_group" "main" {
 # --- EBS CSI Driver Add-on ---
 
 resource "aws_eks_addon" "ebs_csi" {
-  cluster_name = aws_eks_cluster.main.name
-  addon_name   = "aws-ebs-csi-driver"
+  cluster_name  = aws_eks_cluster.main.name
+  addon_name    = "aws-ebs-csi-driver"
+  addon_version = "v1.38.1-eksbuild.1"
 
   depends_on = [aws_eks_node_group.main]
 
